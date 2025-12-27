cab99624f8430bd7059dcff66f523d67fc378dc7
dan-gheorghita <79468719+dan-gheorghita@users.noreply.github.com>
2025-12-27 10:50:11 +0200

    Add Python script

---
+++ helloFunc.py
@@ -0,0 +1,14 @@
+```python
+def hello():
+    # Prints a friendly greeting to the console
+    print('Howdy!')
+    # Prints an enthusiastic version of the greeting
+    print('Howdy!!!')
+    # Prints an alternative greeting
+    print('Hello there.')
+
+# Calls the hello function three times to test its behavior
+hello()
+hello()
+hello()
+```
